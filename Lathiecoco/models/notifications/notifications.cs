e5c7c45fee5117cb9087784ad7f32aa2648141e2
Ibrahima Balde <ibalde@nafafs.com>
2025-02-25 13:07:42 +0000

    mise en place SMS Notification

---
+++ Lathiecoco/models/notifications/notifications.cs
@@ -0,0 +1,21 @@
+﻿namespace Lathiecoco.models.notifications
+{
+    public class Notifications
+    {
+        public string? status { get; set; }
+        public string? message { get; set; }
+        public TransactionData? transactionData { get; set; }
+    }
+
+    public class TransactionData
+    {
+        public string? type { get; set; }
+        public string? peerId { get; set; }
+        public string? peerIdType { get; set; }
+        public double amount { get; set; }
+        public string? currency { get; set; }
+        public string? posId { get; set; }
+        public string? transactionId { get; set; }
+    }
+
+}
---
+++ Lathiecoco/models/notifications/sms.cs
@@ -0,0 +1,13 @@
+﻿namespace Lathiecoco.models.notifications
+{
+    public class sms
+    {
+    }
+
+    public class tokenResponse
+    {
+        public string? token_type { get; set; }
+        public string? access_token { get; set; }
+        public int expires_in { get; set; }
+    }
+}
